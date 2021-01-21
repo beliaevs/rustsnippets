4063a30fdec16a2c1da415a7c15c5e2d3b969f3c
Beliaev <beliaevs@gmail.com>
2021-01-22 02:11:25 +0300

    Random generation and reading the guess.

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,3 +1,28 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    let mut guess = String::new();
+    // generate random number
+    let a = 1;
+    let b = 101;
+    let secret_number = rand::thread_rng().gen_range(a .. b);
+    println!("Guessed number is {}", secret_number);
+    println!("Enter the number");
+    loop
+    {
+        guess.clear();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        let guess_num: u32 = guess.trim().parse().expect("Please type a number!");
+        match guess_num.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!"); 
+                break;
+            }
+        }
+    }
+
 }
